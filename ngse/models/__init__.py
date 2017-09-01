04bb17216c85de272120faee18b3778983fc12fe
pakulin <pakulindev@gmail.com>
2017-09-01 15:03:51 +0300

    move models to module

---
+++ ngse/models/__init__.py
@@ -0,0 +1,16 @@
+from .base import Base
+
+from .answer import Answer
+from .applicant_attribute import ApplicantAttribute
+
+from .category import Category
+from .category_status import CategoryStatus
+
+from .element import Element
+
+from .form import Form
+from .form_type import form_category_association
+from .form_type import FormType
+
+from .user import User
+from .user_type import UserType
